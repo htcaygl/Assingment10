9023cf037d03da7fbaa8d4f98e056b3d560ffd81
htcaygl <80787334+htcaygl@users.noreply.github.com>
2021-04-30 11:39:46 +0300

    Add files via upload

--- Assignment10/Question27.java
+++ Assignment10/Question27.java
@@ -2,23 +2,29 @@ package assignment10;
 
 public class Question27 {
 
+	
 	public static void main(String[] args) {
-		System.out.println(coverString("java methods","me"));
-		System.out.println(coverString("Certified Wooden Spoon","o"));
+		System.out.println(coverString("java methods", "me"));
+		System.out.println(coverString("Certified Wooden Spoon", "o"));
+		System.out.println(coverString("hello hello", "ello"));
+		System.out.println(coverString("apples", "pears"));
 
 	}
 
 	public static String coverString(String s1, String s2) {
-		
-		String result ="";
-		if(s1.contains(s2)) {
-			
-			int index=s1.indexOf(s2);
-			
-			result+=s1.substring(0, index).concat("[").concat(s2).concat("]")+s1.substring(index+s2.length());
-		}
-		
-		return result;
-	}
+		if (s1.contains(s2)) {
 
+			String newS2 = "[" + s2 + "]";
+
+			s1 = s1.replaceAll(s2, newS2);
+		} else {
+
+			s1 = "[" + s1 + "]";
+
+		}
+
+		return s1;
+
+	}
+	
 }
